1df2799f8d820a2d3b4e30a40ae5b15223f89c2b
Elena Liubimova <elena.liubimova3@gmail.com>
2023-05-28 13:47:06 +0300

    feat: добавить инфо о себе

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -39,10 +39,74 @@ const Portfolio = () => {
           <a
             className="portfolio__link"
             target="_blank"
+            href="https://danakun.github.io/competition/"
+          >
+            <span className="portfolio__link-text">
+              Адаптивный сайт (командная работа)
+            </span>
+            <img
+              className="portfolio__arrow"
+              src={arrow}
+              alt="иконка-стрелка"
+            />
+          </a>
+        </li>
+        <li className="portfolio__element">
+          <a
+            className="portfolio__link"
+            target="_blank"
             href="https://elenaliubimova.github.io/mesto-react/"
           >
             <span className="portfolio__link-text">
-              Одностраничное приложение
+              Одностраничное приложение (React)
+            </span>
+            <img
+              className="portfolio__arrow"
+              src={arrow}
+              alt="иконка-стрелка"
+            />
+          </a>
+        </li>
+        <li className="portfolio__element">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            href="https://weather-app2.surge.sh/"
+          >
+            <span className="portfolio__link-text">
+              Одностраничное приложение (React, geolocation)
+            </span>
+            <img
+              className="portfolio__arrow"
+              src={arrow}
+              alt="иконка-стрелка"
+            />
+          </a>
+        </li>
+        <li className="portfolio__element">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            href="https://helpful-queijadas-408f30.netlify.app/"
+          >
+            <span className="portfolio__link-text">
+              Одностраничное приложение (Typescript, React, Redux Toolkit)
+            </span>
+            <img
+              className="portfolio__arrow"
+              src={arrow}
+              alt="иконка-стрелка"
+            />
+          </a>
+        </li>
+        <li className="portfolio__element">
+          <a
+            className="portfolio__link"
+            target="_blank"
+            href="https://elenaliubimova.github.io/snake-game/"
+          >
+            <span className="portfolio__link-text">
+              Игра "Змейка" (Typescript, ООП)
             </span>
             <img
               className="portfolio__arrow"
